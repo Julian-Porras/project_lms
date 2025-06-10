867216e941f144f7a5001dd9f3ad3d1a9948def3
Julian <porrasjulian10@gmail.com>
2025-06-10 17:14:51 +0800

    minor update

--- FRONTEND/src/pages/components/ModuleComponent.jsx
+++ FRONTEND/src/pages/components/ModuleComponent.jsx
@@ -83,102 +83,126 @@ export function ContentComponent({ groupView, contentData }) {
     return (
         <div className="flex flex-col text-base">
             {groupView ?
-                <>
-                    {lectures.length > 0 &&
-                        <>
-                            <ModuleHeaderItemCard>
-                                <LuNewspaper size={18} /><p>LECTURES</p>
-                            </ModuleHeaderItemCard>
-                            {
-                                lectures?.map((lecture) => (
-                                    <Fragment key={lecture.id}>
-                                        <ModuleItemCard>
-                                            <div className={styles.contentItemWrapperGroup}>
-                                                <p key={lecture.id} className="hover:underline cursor-pointer" >{lecture.name}</p>
-                                            </div>
-                                            <div className={styles.contentItemWrapper}>
-                                                {lecture.visible ? <PublishedContentStatus /> : <NotPublishedContentStatus />}
-                                                <SettingsIcon />
-                                            </div>
-                                        </ModuleItemCard>
-                                        <Divider />
-                                    </Fragment>
-                                ))
-                            }
-                        </>
-                    }
-                    {assignments.length > 0 &&
-                        <>
-                            <ModuleHeaderItemCard>
-                                <LuNotebookPen size={18} /> <p>ASSIGNMENTS</p>
-                            </ModuleHeaderItemCard>
-                            {
-                                assignments?.map((assignment) => (
-                                    <Fragment key={assignment.id}>
-                                        <ModuleItemCard>
-                                            <div className={styles.contentItemWrapperGroup}>
-                                                <p key={assignment.id} className=" hover:underline cursor-pointer" >{assignment.name}</p>
-                                            </div>
-                                            <div className={styles.contentItemWrapper}>
-                                                {assignment.visible ? <PublishedContentStatus /> : <NotPublishedContentStatus />}
-                                                <SettingsIcon />
-                                            </div>
-                                        </ModuleItemCard>
-                                        <Divider />
-                                    </Fragment>
-                                ))
-                            }
-                        </>
-                    }
-                    {quizzes.length > 0 &&
-                        <>
-                            <ModuleHeaderItemCard>
-                                <LuRocket size={18} /><p>QUIZZES</p>
-                            </ModuleHeaderItemCard>
-                            {
-                                quizzes?.map((quiz) => (
-                                    <Fragment key={quiz.id}>
-                                        <ModuleItemCard>
-                                            <div className={styles.contentItemWrapperGroup}>
-                                                <p key={quiz.id} className=" hover:underline cursor-pointer" >{quiz.name}</p>
-                                            </div>
-                                            <div className={styles.contentItemWrapper}>
-                                                {quiz.visible ? <PublishedContentStatus /> : <NotPublishedContentStatus />}
-                                                <SettingsIcon />
-                                            </div>
-                                        </ModuleItemCard>
-                                        <Divider />
-                                    </Fragment>
-                                ))
-                            }
-                        </>
-                    }
-                </>
+                <GroupContentLayout
+                    lectures={lectures}
+                    assignments={assignments}
+                    quizzes={quizzes}
+                />
                 :
-                contentData?.map((item) => (
-                    <Fragment key={item.id}>
-                        <ModuleItemCard >
-                            <div className={styles.contentItemWrapper}>
-                                {item.item_type === CONTENT.LECTURE &&
-                                    <LuNewspaper color="#168f56" size={18} />
-                                }
-                                {item.item_type === CONTENT.ASSIGNMENT &&
-                                    <LuNotebookPen color="#168f56" size={18} />
-                                }
-                                {item.item_type === CONTENT.QUIZ &&
-                                    <LuRocket color="#168f56" size={18} />
-                                }
-                                <p className=" hover:underline cursor-pointer" >{item.item_name}</p>
-                            </div>
-                            <div className={styles.contentItemWrapper}>
-                                {item.is_visible ? <PublishedContentStatus /> : <NotPublishedContentStatus />}
-                                <SettingsIcon />
-                            </div>
-                        </ModuleItemCard>
-                        <Divider />
-                    </Fragment>
-                ))
+                <ListContentLayout
+                    contentData={contentData}
+                />
             }
         </div>
     );
+}
+
+export function ListContentLayout({
+    contentData
+}) {
+    return (
+        contentData?.map((item) => (
+            <Fragment key={item.id}>
+                <ModuleItemCard >
+                    <div className={styles.contentItemWrapper}>
+                        {item.item_type === CONTENT.LECTURE &&
+                            <LuNewspaper color="#168f56" size={18} />
+                        }
+                        {item.item_type === CONTENT.ASSIGNMENT &&
+                            <LuNotebookPen color="#168f56" size={18} />
+                        }
+                        {item.item_type === CONTENT.QUIZ &&
+                            <LuRocket color="#168f56" size={18} />
+                        }
+                        <p className=" hover:underline cursor-pointer" >{item.item_name}</p>
+                    </div>
+                    <div className={styles.contentItemWrapper}>
+                        {item.is_visible ? <PublishedContentStatus /> : <NotPublishedContentStatus />}
+                        <SettingsIcon />
+                    </div>
+                </ModuleItemCard>
+                <Divider />
+            </Fragment>
+        ))
+    )
+}
+
+export function GroupContentLayout({
+    lectures,
+    assignments,
+    quizzes,
+}) {
+    return (
+        <>
+            {lectures.length > 0 &&
+                <>
+                    <ModuleHeaderItemCard>
+                        <LuNewspaper size={18} /><p>LECTURES</p>
+                    </ModuleHeaderItemCard>
+                    {
+                        lectures?.map((lecture) => (
+                            <Fragment key={lecture.id}>
+                                <ModuleItemCard>
+                                    <div className={styles.contentItemWrapperGroup}>
+                                        <p key={lecture.id} className="hover:underline cursor-pointer" >{lecture.name}</p>
+                                    </div>
+                                    <div className={styles.contentItemWrapper}>
+                                        {lecture.visible ? <PublishedContentStatus /> : <NotPublishedContentStatus />}
+                                        <SettingsIcon />
+                                    </div>
+                                </ModuleItemCard>
+                                <Divider />
+                            </Fragment>
+                        ))
+                    }
+                </>
+            }
+            {assignments.length > 0 &&
+                <>
+                    <ModuleHeaderItemCard>
+                        <LuNotebookPen size={18} /> <p>ASSIGNMENTS</p>
+                    </ModuleHeaderItemCard>
+                    {
+                        assignments?.map((assignment) => (
+                            <Fragment key={assignment.id}>
+                                <ModuleItemCard>
+                                    <div className={styles.contentItemWrapperGroup}>
+                                        <p key={assignment.id} className=" hover:underline cursor-pointer" >{assignment.name}</p>
+                                    </div>
+                                    <div className={styles.contentItemWrapper}>
+                                        {assignment.visible ? <PublishedContentStatus /> : <NotPublishedContentStatus />}
+                                        <SettingsIcon />
+                                    </div>
+                                </ModuleItemCard>
+                                <Divider />
+                            </Fragment>
+                        ))
+                    }
+                </>
+            }
+            {quizzes.length > 0 &&
+                <>
+                    <ModuleHeaderItemCard>
+                        <LuRocket size={18} /><p>QUIZZES</p>
+                    </ModuleHeaderItemCard>
+                    {
+                        quizzes?.map((quiz) => (
+                            <Fragment key={quiz.id}>
+                                <ModuleItemCard>
+                                    <div className={styles.contentItemWrapperGroup}>
+                                        <p key={quiz.id} className=" hover:underline cursor-pointer" >{quiz.name}</p>
+                                    </div>
+                                    <div className={styles.contentItemWrapper}>
+                                        {quiz.visible ? <PublishedContentStatus /> : <NotPublishedContentStatus />}
+                                        <SettingsIcon />
+                                    </div>
+                                </ModuleItemCard>
+                                <Divider />
+                            </Fragment>
+                        ))
+                    }
+                </>
+            }
+        </>
+    )
 }
